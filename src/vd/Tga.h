f21fd8d142250180995062b565b7c532b5d322bc
Ivan Romanov <iwromanov@gmail.com>
2024-11-20 07:54:28 +0300

    Code to work with TGA placed in separate file.

---
+++ src/vd/Tga.h
@@ -0,0 +1,15 @@
+#ifndef VD_TGA_H_
+#define VD_TGA_H_
+
+#include <iostream>
+#include <vector>
+
+namespace vd
+{
+
+//Writes pixels as is, so pixels representation must be little-endian ARGB
+void WriteTga(std::ostream &stream, const std::vector<std::byte> &pixels, std::size_t width);
+
+}//namespace vd
+
+#endif //VD_TGA_H_

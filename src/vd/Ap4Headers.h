07c33ec2c45a53bd3df91b1697a56c74b5d8de64
Ivan Romanov <iwromanov@gmail.com>
2024-12-31 20:13:07 +0300

    Preprocessor improvements.

--- src/vd/Ap4Headers.h
+++ src/vd/Ap4Headers.h
@@ -1,18 +1,20 @@
 #ifndef VDOWNLOADER_VD_AP4_HEADERS_
 #define VDOWNLOADER_VD_AP4_HEADERS_
 
-#if defined(_MSC_VER)
+#include "Preprocessor.h"
+
+#if defined(VDOWNLOADER_COMPILER_MSVC)
     #pragma warning(push, 0)
-#elif defined(__GNUG__)
+#elif defined(VDOWNLOADER_COMPILER_GCC)
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wunused-parameter"
 #endif
 
 #include <Ap4.h>
 
-#if defined(_MSC_VER)
+#if defined(VDOWNLOADER_COMPILER_MSVC)
     #pragma warning(pop)
-#elif defined(__GNUG__)
+#elif defined(VDOWNLOADER_COMPILER_GCC)
     #pragma GCC diagnostic pop
 #endif
 

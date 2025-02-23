fe431b34a329b65eb775958ac989a3874dc2e8d1
Ivan Romanov <iwromanov@gmail.com>
2025-02-23 23:57:04 +0300

    Added valgrind check to cmake and CI

---
+++ cmake/valgrind.cmake
@@ -0,0 +1,10 @@
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+    add_custom_target(valgrind_check
+                      COMMAND valgrind
+                              --leak-check=full
+                              --error-exitcode=1
+                              "\"$<TARGET_FILE:${PROJECT_NAME}>\""
+                              -t 2 "\"${PROJECT_SOURCE_DIR}/tests/data/squares.mp4\"" 0s-3s:2 4s-7s:2 8s-11s:2 12s-15s:2
+                      COMMENT "Running valgrind check")
+    add_dependencies(valgrind_check ${PROJECT_NAME})
+endif()
